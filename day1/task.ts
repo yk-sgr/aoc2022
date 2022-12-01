759cd4e9e174a0c8da9f938ffe4d5bc99d1af617
Yannick Seeger <yannick.sgr@gmail.com>
2022-12-01 14:56:14 +0100

    day1

---
+++ day1/task.ts
@@ -0,0 +1,8 @@
+const input = await Deno.readTextFile("input.txt");
+
+const elves = input.split("\n\n")
+
+const elveCalories = elves.map((elve) => elve.split("\n").map((calorie) => parseInt(calorie)).reduce((a, b) => a+b)).sort((a, b) => b - a);
+
+console.log(elveCalories[0]);
+console.log(elveCalories[0] + elveCalories[1] + elveCalories[2]);
