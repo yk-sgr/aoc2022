fcb056027870ac966ca9f6d0eb6d505ae035d98b
Yannick Seeger <yannick.sgr@gmail.com>
2022-12-03 13:11:10 +0100

    Add .gitignore

--- day1/task.ts
+++ day1/task.ts
@@ -1,6 +1,6 @@
 const input = await Deno.readTextFile("input.txt");
 
-const elves = input.split("\n\n")
+const elves = input.split("\n\n");
 
 const elveCalories = elves.map((elve) => elve.split("\n").map((calorie) => parseInt(calorie)).reduce((a, b) => a+b)).sort((a, b) => b - a);
 
